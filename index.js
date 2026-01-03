5ab4d9e8e76e73ec5ed6e54c738da4edd4059720
BrundaBR <revadibrunda@gmail.com>
2026-01-03 15:13:03 +0000

    Second section

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ ham.addEventListener("click", ()=>{
     ham.classList.toggle("active");
     nav_links.classList.toggle("active");
 });
+
+
+const cards = document.querySelector('.cards');
+const card = document.querySelector('.card');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let index = 0;
+const cardWidth = card.offsetWidth +20;
+
+
+next.addEventListener('click', () => {
+  if (index < cards.children.length - 1) {
+    index++;
+    cards.style.transform = `translateX(-${index * cardWidth}px)`;
+  }
+});
+
+prev.addEventListener('click', () => {
+  if (index > 0) {
+    index--;
+    cards.style.transform = `translateX(-${index * cardWidth}px)`;
+  }
+});
